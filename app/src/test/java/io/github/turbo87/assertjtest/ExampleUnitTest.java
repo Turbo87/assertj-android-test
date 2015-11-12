5dad6177bb779fd1c2b2b2bb6088e51b54b8c34c
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-11-12 11:55:46 +0100

    Test that assertThatThrownBy() works too

--- app/src/test/java/io/github/turbo87/assertjtest/ExampleUnitTest.java
+++ app/src/test/java/io/github/turbo87/assertjtest/ExampleUnitTest.java
@@ -1,8 +1,10 @@
 package io.github.turbo87.assertjtest;
 
+import org.assertj.core.api.ThrowableAssert;
 import org.junit.Test;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.assertj.core.api.Java6Assertions.assertThatThrownBy;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
@@ -11,5 +13,17 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertThat(2 + 2).isEqualTo(4);
+    }
+
+    @Test
+    public void testExceptions() throws Exception {
+        assertThatThrownBy(new ThrowableAssert.ThrowingCallable() {
+            @Override
+            public void call() throws Throwable {
+                throw new IllegalArgumentException("foo");
+            }
+        })
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("foo");
     }
 }
